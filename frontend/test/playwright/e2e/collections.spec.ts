db24dd35be4f26183c8ada04cc0a318ed1ddce40
Olga Bulat <obulat@gmail.com>
2024-01-14 15:25:51 +0300

    fix collections.spec.ts
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/test/playwright/e2e/collections.spec.ts
+++ frontend/test/playwright/e2e/collections.spec.ts
@@ -1,8 +1,13 @@
-import { test, expect } from "@playwright/test"
+import { test, expect, Page } from "@playwright/test"
 
 import { preparePageForTests, t } from "~~/test/playwright/utils/navigation"
 
 test.describe.configure({ mode: "parallel" })
+
+const getH1 = (page: Page, text: string | RegExp) =>
+  page.getByRole("heading", { level: 1, name: text })
+const loadMoreButton = (page: Page) =>
+  page.getByRole("button", { name: t("browsePage.load") })
 
 test.describe("collections", () => {
   test.beforeEach(async ({ page }) => {
@@ -12,18 +17,15 @@ test.describe("collections", () => {
       },
     })
     await page.goto("/image/f9384235-b72e-4f1e-9b05-e1b116262a29?q=cat")
+    await expect(getH1(page, /cat/i)).toBeVisible()
   })
 
   test("can open tags collection page from image page", async ({ page }) => {
     // Using the href because there are multiple links with the same text.
     await page.click('[href*="/tag/cat"]')
 
-    await expect(
-      page.getByRole("button", { name: t("browsePage.load") })
-    ).toBeEnabled()
-    await expect(
-      page.getByRole("heading", { level: 1, name: /cat/i })
-    ).toBeVisible()
+    await expect(getH1(page, /cat/i)).toBeVisible()
+    await expect(loadMoreButton(page)).toBeEnabled()
     expect(await page.locator("figure").count()).toEqual(20)
     expect(page.url()).toMatch(/image\/tag\/cat/)
   })
@@ -33,12 +35,8 @@ test.describe("collections", () => {
 
     await page.getByRole("link", { name: sourcePattern }).first().click()
 
-    await expect(
-      page.getByRole("button", { name: t("browsePage.load") })
-    ).toBeEnabled()
-    await expect(
-      page.getByRole("heading", { name: sourcePattern })
-    ).toBeVisible()
+    await expect(loadMoreButton(page)).toBeEnabled()
+    await expect(getH1(page, sourcePattern)).toBeVisible()
     expect(await page.locator("figure").count()).toEqual(20)
     expect(page.url()).toMatch(/image\/source\/flickr\/$/)
   })
@@ -47,12 +45,8 @@ test.describe("collections", () => {
     const creatorPattern = /strogoscope/i
     await page.getByRole("link", { name: creatorPattern }).first().click()
 
-    await expect(
-      page.getByRole("button", { name: t("browsePage.load") })
-    ).toBeEnabled()
-    await expect(
-      page.getByRole("heading", { level: 1, name: creatorPattern })
-    ).toBeVisible()
+    await expect(loadMoreButton(page)).toBeEnabled()
+    await expect(getH1(page, creatorPattern)).toBeVisible()
     expect(await page.locator("figure").count()).toEqual(20)
     expect(page.url()).toMatch(/image\/source\/flickr\/creator\/strogoscope\//)
   })
